3ff141a7d3e813fc32465da246b66f2ed4119cf8
David Houston <houstdav000@gmail.com>
2020-10-29 10:34:32 -0400

    Update dotfiles

--- shells/bash.nix
+++ shells/bash.nix
@@ -10,8 +10,10 @@
   # Source in environment variables file
   home.file.".profile".source = ../dotfiles/.profile;
   home.file.".bash_profile".source = ../dotfiles/.bash_profile;
+  home.file.".bashrc".source = ../dotfiles/.bashrc;
 
   # Copy .config/bash
-  xdg.configFile."bash/config.sh".source = ../dotfiles/.config/bash/config.sh;
-  xdg.configFile."bash/alias.sh".source = ../dotfiles/.config/bash/alias.sh;
+  xdg.configFile."bash/config.bash".source = ../dotfiles/.config/bash/config.bash;
+  xdg.configFile."bash/alias.bash".source = ../dotfiles/.config/bash/alias.bash;
+  xdg.configFile."bash/functions.bash".source = ../dotfiles/.config/bash/functions.bash
 }
