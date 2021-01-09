d6b06430489a6232102c97286dd267c715bac0d8
David Houston <houstdav000@gmail.com>
2021-01-08 19:41:33 -0500

    Add zplug plugins to zsh

--- shells/zsh.nix
+++ shells/zsh.nix
@@ -9,4 +9,5 @@
   xdg.configFile."zsh/config.zsh".source = ../dotfiles/.config/zsh/config.zsh;
   xdg.configFile."zsh/alias.zsh".source = ../dotfiles/.config/zsh/alias.zsh;
   xdg.configFile."zsh/functions.zsh".source = ../dotfiles/.config/zsh/functions.zsh;
+  xdg.configFile."zsh/plugin.zsh".source = ../dotfiles/.config/zsh/plugin.zsh;
 }
